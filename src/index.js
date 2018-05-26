f35ec1e42080bf770dc7a804516c84d197637678
Alexis Robillart <alexisrobillart@gmail.com>
2018-05-26 19:22:14 +0200

    add project factory function

--- src/index.js
+++ src/index.js
@@ -4,5 +4,25 @@ import {
   formatRelative,
   subDays
 } from 'date-fns';
+// console.log(format(new Date(2014, 1, 11), 'MM/DD/YYYY'));
 
-console.log(format(new Date(2014, 1, 11), 'MM/DD/YYYY'));
+const project = (function () {
+
+  const createProject = (name) => {
+    let listOfTasks = [];
+    return {
+      name,
+      listOfTasks,
+    };
+  };
+
+  return {
+    createProject,
+  }
+
+})();
+
+
+const basic = project.createProject("Basique");
+
+console.log(basic);
